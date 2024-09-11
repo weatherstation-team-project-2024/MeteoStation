65a2242d699499f48f9c528c9a28a6870856c95d
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2024-09-11 16:20:33 +0200

    chore: component-setup

---
+++ client/src/app/components/data-view/data-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-view.component.html',
+  styleUrl: './data-view.component.css'
+})
+export class DataViewComponent {
+
+}
